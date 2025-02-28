417b5c51c882a68ad54d29f0750b23adaabdcd43
Isabella <isabellaotoo25@gmail.com>
2025-02-28 11:58:14 +0000

    Add jobSlice to redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import jobReducer from '../features/jobSlice';
+import userReducer from '../features/userSlice';
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        user: userReducer,
+        jobs: jobReducer,
+    },
 
 });
 
