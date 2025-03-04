73e3d5de1c3255a5039e0077753eae0f0f4aca58
Isabella <isabellaotoo25@gmail.com>
2025-03-04 23:35:40 +0000

    Udate JobForm

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from '../redux/store'
+import { removeJob } from '../features/jobSlice'
 
 const JobList = () => {
+    const jobs = useSelector((state: RootState) => state.jobs.jobs);
+    const dispatch = useDispatch();
   return (
     <div>
       
