d03bf87cccc38d6c2510530991589fad563e0a82
Isabella <isabellaotoo25@gmail.com>
2025-03-20 21:31:55 +0000

    Update middleware

--- devconnect-backend/middleware/authMiddleware.ts
+++ devconnect-backend/middleware/authMiddleware.ts
@@ -1,2 +1,10 @@
 import jwt from "jsonwebtoken";
-import  { Request, Response, NextFunction } from "express"
+import { Request, Response, NextFunction } from "express"
+
+export interface AuthRequest extends Request { 
+    user?: any;
+}
+
+export const authenticate = (req: AuthRequest, res: Response, next: NextFunction) => {
+    const token = req.header("Authorization")?.split(" ")[1];
+}
