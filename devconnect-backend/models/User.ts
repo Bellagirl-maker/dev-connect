3e96335cccc256102177fb8f382df9228aa88c00
Isabella <isabellaotoo25@gmail.com>
2025-03-24 11:24:19 +0000

    Update User model

--- devconnect-backend/models/User.ts
+++ devconnect-backend/models/User.ts
@@ -1,19 +1,32 @@
 import mongoose, { Schema, Document } from 'mongoose';
-import bycrypt from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 
 export interface IUser extends Document {
     name: string;
     email: string;
     password: string;
-    comparedPassword: (password: string) => Promise<boolean>;
+    comparePassword: (password: string) => Promise<boolean>;
 }
 
-const userSchema: Schema = new Schema(
+const UserSchema: Schema = new Schema(
     {
         name: { type: String, required: true },
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true }
     },
-    
+
     { timestamps: true }
-)
+);
+
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash( this.password, salt);
+    next();
+});
+
+UserSchema.methods.comparePassword = async function (password: string) {
+    return bcrypt.compare(password, this.password);
+  };
+  
+  export default mongoose.model<IUser>("User", UserSchema);
