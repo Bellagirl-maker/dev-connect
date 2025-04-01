b7fcececb14e33d982bd26556838557e5bb9a81c
Isabella <isabellaotoo25@gmail.com>
2025-04-01 12:07:33 +0000

    Integrate API Service with Redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import jobReducer from '../features/jobSlice';
 import userReducer from '../features/userSlice';
+import { authApi } from "../services/authApi";
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
         jobs: jobReducer,
+        [authApi.reducerPath]: authApi.reducer
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(authApi.middleware)
 
 });
 
