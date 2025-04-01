87360bd262a6f865ff8be9d380ac95541577c2e3
Isabella <isabellaotoo25@gmail.com>
2025-04-01 11:56:25 +0000

    Update service

--- src/services/authApi.ts
+++ src/services/authApi.ts
@@ -6,15 +6,30 @@ const API_URL = 'https://localhost:5000/api'
 export const authApi = createApi({
     reducerPath: "authApi",
     baseQuery: fetchBaseQuery({ baseUrl: API_URL }),
-    endpoints: (builder) => {
+    endpoints: (builder) => ({
         loginUser: builder.mutation({
             query: (userData) => ({
                 url: "/auth/login",
                 method: "POST",
-                body: userData,    
+                body: userData,
                 
             }),
         }),
+            registerUser: builder.mutation({
+                query: (userData) => ({
+                    url: "/auth/register",
+                    method: "POST",
+                    body: userData,
+                }),
+
+            }),
+                getJobs: builder.query({
+                    query: (token) => ({
+                        url: "/jobs",
+                        headers: { Authorization: `Bearer ${token}` }
+                    }),
+                }),
     
-    }
-})
+    }),
+});
+export const { useLoginUserMutation, useRegisterUserMutation, useGetJobsQuery } = authApi;
