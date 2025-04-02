c06218d1f787091a127093eb64ab22af6d6e191a
Isabella <isabellaotoo25@gmail.com>
2025-04-02 21:09:15 +0000

    Update authentication Slice

--- src/features/authSlice.ts
+++ src/features/authSlice.ts
@@ -19,6 +19,14 @@ const authSlice = createSlice({
             state.user = action.payload.user;
             localStorage.setItem("token", action.payload.token);
             
-        }
-    }
-})
+        },
+        logout: (state) => {
+            state.token = null;
+            state.user = null;
+            localStorage.removeItem("token");
+        },
+    },
+});
+
+export const { setCredentials, logout } = authSlice.actions;
+export default authSlice.reducer;
