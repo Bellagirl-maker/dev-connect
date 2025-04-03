d114993c8cb929daa67a0722b771a9216e84abec
Isabella <isabellaotoo25@gmail.com>
2025-04-03 21:38:55 +0000

    Update Login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,5 +4,9 @@ import { useDispatch } from 'react-redux';
 import { setCredentials } from '../features/authSlice';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loginUser] = useLoginUserMutation();
+    const dispatch = useDispatch();
     
 }
