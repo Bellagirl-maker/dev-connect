79c26f1206fd471e7e30821bd967f174c306eca8
Isabella <isabellaotoo25@gmail.com>
2025-04-08 20:35:34 +0000

    Update login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -24,6 +24,8 @@ const Login = () => {
             <h2>LOGIN</h2>
             <form onSubmit={handleLogin}>
                 <input type='email' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)} required />
+                <input type='password' placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} required />
+                <button type='submit'>Login</button>
             </form>
         </div>
     )
