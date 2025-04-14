1f8b2fc4b7996f6fe413e39d871b0d21bfe7d068
Isabella <isabellaotoo25@gmail.com>
2025-04-14 21:34:10 +0000

    Update  Jobs Page

--- src/pages/Jobs.tsx
+++ src/pages/Jobs.tsx
@@ -2,3 +2,8 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
 import { useGetJobsQuery } from '../services/authApi';
+
+const Jobs = () => {
+    const token = useSelector((state: RootState) => state.auth.token);
+    const { data: Jobs, error } = useGetJobsQuery(token);
+}
